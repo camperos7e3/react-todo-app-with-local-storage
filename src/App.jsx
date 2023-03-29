4f0219eb32ee4a55aa2bb2005be1ba746f1e7dbd
camperos7e3 <camperos.7e3@proton.me>
2023-03-29 17:18:05 +0200

    create app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,78 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+// custom hooks
+import useLocalStorage from './hooks/useLocalStorage'
+
+// custom components
+import CustomForm from './components/CustomForm'
+import EditForm from './components/EditForm'
+import TaskList from './components/TaskList'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useLocalStorage('react-todo.tasks', [])
+  const [previousFocusEl, setPreviousFocusEl] = useState(null)
+  const [editedTask, setEditedTask] = useState(null)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const addTask = (task) => {
+    setTasks(prevState => [...prevState, task])
+  }
+
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(t => t.id !== id))
+  }
+
+  const toggleTask = (id) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === id
+        ? { ...t, checked: !t.checked }
+        : t
+    )))
+  }
+
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === task.id
+        ? { ...t, name: task.name }
+        : t
+    )))
+    closeEditMode()
+  }
+
+  const closeEditMode = () => {
+    setIsEditing(false)
+    previousFocusEl.focus()
+  }
+
+  const enterEditMode = (task) => {
+    setEditedTask(task)
+    setIsEditing(true)
+    setPreviousFocusEl(document.activeElement)
+  }
 
   return (
-    <div className='App'>
-      <div>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='container'>
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      {
+        isEditing && (
+          <EditForm
+            editedTask={editedTask}
+            updateTask={updateTask}
+            closeEditMode={closeEditMode}
+          />
+        )
+      }
+      <CustomForm addTask={addTask} />
+      {tasks && (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      )}
     </div>
   )
 }
